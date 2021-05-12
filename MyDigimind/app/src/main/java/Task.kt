28c7160dd13bf67ed8db82486282f2f34a71a61f
TheR2D2cx <63327126+TheR2D2cx@users.noreply.github.com>
2021-05-11 21:06:45 -0800

    login con firebase

--- MyDigimind/app/src/main/java/Task.kt
+++
@@ -1,2 +0,0 @@
-data class Task(var title:String, var days: ArrayList<String>,var time:String) {
-}
--- MyDigimind/app/src/main/java/jorge/mendoza/mydigimind/Recordatorio.kt
+++
@@ -1,6 +0,0 @@
-package jorge.mendoza.mydigimind
-
-import java.io.Serializable
-
-data class Recordatorio(var dias: String, var tiempo: String, var nombre: String) : Serializable {
-}
